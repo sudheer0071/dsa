8d5854cb0765acb96d13592d1d4b391f875ba5c8
sudheer0071 <sudheer1614@gmail.com>
2024-02-22 22:59:59 +0530

    .

--- Binary Search/leetcode/q2.cpp
+++ Binary Search/leetcode/q2.cpp
@@ -25,8 +25,7 @@ int main()
   // int a[6] = {2,4,6,9,14,20};
   // int b[5] = {0,10,8,5,3};
   int b[5] = {7, 9, 10, 2, 3};
-
-  // int ind = binaraSearch(a,6, 4);
+ 
   int ind = peak(b, 5);
   cout << "Index of peak element is :" << ind << endl;
   return 0;

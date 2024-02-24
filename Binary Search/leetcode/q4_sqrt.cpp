033e1614aa02dcf8abbbba7a4aa42ab8fca2a3c5
sudheer0071 <sudheer1614@gmail.com>
2024-02-24 18:20:29 +0530

    sqrt using binary search done.

--- Binary Search/leetcode/q4_sqrt.cpp
+++ Binary Search/leetcode/q4_sqrt.cpp
@@ -1,55 +1,56 @@
 #include <iostream>
 using namespace std;
- 
-long long int sqrtt(int n){ 
+
+long long int sqrtt(int n)
+{
   int arr[n];
-  int s= 0;
+  int s = 0;
   int e = n;
-  long long int mid = s + (e-s)/2;
+  long long int mid = s + (e - s) / 2;
   long long int ans;
-  while (s<=e)
-  { 
-    if (mid*mid == n)
+  while (s <= e)
+  {
+    if (mid * mid == n)
     {
       return mid;
     }
-    
-    if (mid*mid<n)
+
+    if (mid * mid < n)
     {
       ans = mid;
-      s = mid+1;
-    } 
-    else 
+      s = mid + 1;
+    }
+    else
     {
-      e = mid -1;
-    } 
-    mid = s + (e-s)/2;
+      e = mid - 1;
+    }
+    mid = s + (e - s) / 2;
   }
   return ans;
 }
 
-double more_precision(int n, int precesion, int temp){ 
-   double factor = 1;
-   double ans = temp;
+double more_precision(int n, int precesion, int temp)
+{
+  double factor = 1;
+  double ans = temp;
 
-   for (int i = 0; i < precesion; i++)
-   {
-    factor = factor/10;
-    for (double j = ans; j*j < n; j= j+factor)
+  for (int i = 0; i < precesion; i++)
+  {
+    factor = factor / 10;
+    for (double j = ans; j * j < n; j = j + factor)
     {
       ans = j;
     }
-    
-   }
-   return ans;
   }
+  return ans;
+}
 
 int main()
 {
   int a;
-  cin >>a;
+  cin >> a;
   int temp = sqrtt(a);
-  cout << "square root of "<< a<< " is: "<< more_precision(a, 2, temp);
+  cout << "square root of " << a << " is: " << more_precision(a, 2, temp);
 
   return 0;
 }

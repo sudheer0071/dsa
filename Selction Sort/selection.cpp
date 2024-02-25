191b581e0588bbfdff03f644221d3b5302b7b19c
sudheer0071 <sudheer1614@gmail.com>
2024-02-25 21:50:44 +0530

    modification in selection sort

--- Selction Sort/selection.cpp
+++ Selction Sort/selection.cpp
@@ -2,10 +2,10 @@
 using namespace std;
  
 int selection_sort(int arr[], int n){
-   for (int i = 0; i < n-1; i++) 
+   for (int i = 0; i < n; i++) 
    {
     int min = i;
-    for (int j = i; j < n; j++)
+    for (int j = i+1; j < n; j++)
     {
       if (arr[j]<arr[min]) 
         min = j;  

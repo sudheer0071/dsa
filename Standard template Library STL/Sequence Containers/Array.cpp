6abbf675da9e97312b07f38472a6a87813890484
sudheer0071 <sudheer1614@gmail.com>
2024-02-27 21:49:27 +0530

    array done

---
+++ Standard template Library STL/Sequence Containers/Array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+ 
+int main()
+{
+  //*************** normal array ***************
+  int basic[4] = {3,5,6,7};
+  //*************** STL array ***************
+ array<int, 4> a = {1,2,3,4};
+int size = a.size();
+ for (int i = 0; i < size; i++)
+ {
+  cout<<a[i]<<endl;
+ }
+ cout<<"element at 2nd index-> "<<a.at(3)<<endl;
+ cout<<"Empty or not->"<<a.empty()<<endl;
+cout<<a.front()<<endl;
+cout<<a.back()<<endl;
+  return 0;
+}
---
+++ Standard template Library STL/Sequence Containers/vector.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+
+// ********** vector is a dynamic array **********
+int main()
+{
+  vector<int> a(5,1);
+   cout<<"Print a"<<endl;
+  for(int i:a){
+  cout<<i<<" ";
+}cout<<endl;
+
+// copy elements of a in array named "last"
+  vector<int> last(a);
+  
+  cout<<"Print last"<<endl;
+  for(int i:last){
+  cout<<i<<" ";
+}cout<<endl;
+  
+  vector<int> v;
+  cout<<"capacity: "<<v.capacity()<<endl;
+  v.push_back(1);
+  cout<<"capacity: "<<v.capacity()<<endl;
+  v.push_back(2);
+  cout<<"capacity: "<<v.capacity()<<endl;
+  v.push_back(3);
+  cout<<"capacity: "<<v.capacity()<<endl; 
+  cout<<"Size: "<<v.size()<<endl; 
+  cout<<"Element at"<<v.at(1)<<endl;
+
+cout<<"front: "<<v.front()<<endl;
+cout<<"before pop"<<endl;
+for(int i:v){
+  cout<<i<<" ";
+}cout<<endl;
+// pop from back
+v.pop_back();
+ 
+cout<<"after pop"<<endl;
+for(int i:v){
+  cout<<i<<" ";
+}cout<<endl;
+
+  cout<<"Before clear: "<<v.size()<<endl; 
+v.clear();
+  cout<<"After clear: "<<v.size()<<endl; 
+
+  return 0;
+}

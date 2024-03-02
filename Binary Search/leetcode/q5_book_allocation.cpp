7b85ac1f32f62c1f53cd59e53fb5ce6e8c3319ad
sudheer0071 <sudheer1614@gmail.com>
2024-03-02 23:24:15 +0530

    structuring binary search

--- Binary Search/leetcode/q5_book_allocation.cpp
+++ Binary Search/leetcode/q5_book_allocation.cpp
@@ -1,60 +1,62 @@
 #include <iostream>
-#include<numeric>
+#include <numeric>
 #include <bits/stdc++.h>
 using namespace std;
- 
-bool is_possible(int arr[], int n, int m, int mid){
-   int studentCount = 1;
-   int pagesum =0;
 
-   for (int i = 0; i < n; i++)
-   {
+bool is_possible(int arr[], int n, int m, int mid)
+{
+  int studentCount = 1;
+  int pagesum = 0;
+
+  for (int i = 0; i < n; i++)
+  {
     if (pagesum + arr[i] <= mid)
     {
-       pagesum += arr[i];
+      pagesum += arr[i];
     }
     else
     {
       studentCount++;
-      if (studentCount >m || arr[i] > mid)
+      if (studentCount > m || arr[i] > mid)
       {
         return false;
       }
       pagesum = arr[i];
     }
-   }
-    return true;
-   
+  }
+  return true;
 }
 
-int allocate_book(int arr[],int n, int m){
-    int s = 0;
-    int sum=0;
-    for (int i = 0; i < n; i++)
+int allocate_book(int arr[], int n, int m)
+{
+  int s = 0;
+  int sum = 0;
+  for (int i = 0; i < n; i++)
+  {
+    sum += arr[i];
+  }
+  int e = sum;
+  int mid = s + (e - s) / 2;
+  int ans;
+  while (s <= e)
+  {
+    if (is_possible(arr, n, m, mid))
     {
-      sum+=arr[i];
+      ans = mid;
+      e = mid - 1;
     }
-    int e = sum;
-    int mid = s+(e-s)/2;
-    int ans;
-    while (s<=e)
+    else
     {
-      if (is_possible(arr,n,m,mid))
-      {
-        ans=mid;
-        e = mid -1;
-      }
-      else{
-        s= mid +1;
-      }
-      mid = s+(e-s)/2;
+      s = mid + 1;
     }
-    return ans;
+    mid = s + (e - s) / 2;
+  }
+  return ans;
 }
 
 int main()
 {
-  int b[5] = {10,20,30,40};
-  cout<< allocate_book(b,4,2);
+  int b[5] = {10, 20, 30, 40};
+  cout << allocate_book(b, 4, 2);
   return 0;
 }

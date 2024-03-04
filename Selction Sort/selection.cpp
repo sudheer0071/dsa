e9b54fae3b09982c05636cf5ba893ca8c1c0d4d4
sudheer0071 <sudheer1614@gmail.com>
2024-03-04 23:26:25 +0530

    .

--- Selction Sort/selection.cpp
+++ Selction Sort/selection.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int selection_sort(int arr[], int n){
    for (int i = 0; i < n; i++) 
    {
-    int min = i;
+    int min = i; 
     for (int j = i+1; j < n; j++)
     {
       if (arr[j]<arr[min]) 
@@ -27,7 +27,7 @@ int main()
   int a[5] = {5,3,1,6,8};
   cout<<"Before sort: ";
    printarray(a,5);
-  
+
   selection_sort(a,5);
   cout<<"After sort: ";
   printarray(a,5);

ac6e49cfebf1af2d37ce366c7a79037a7f06f19c
sudheer0071 <sudheer1614@gmail.com>
2024-03-05 22:02:45 +0530

    .

--- leetcode - codestudio Arrays/RotateArray.cpp
+++ leetcode - codestudio Arrays/RotateArray.cpp
@@ -2,7 +2,7 @@
 using namespace std;
  
 void rotateArray(int arr[],int n, int k){
-  
+
   int temp[n];
   for (int i = 0; i < n; i++)
   {
@@ -17,6 +17,7 @@ void rotateArray(int arr[],int n, int k){
     cout<<arr[i] << " ";
    }
  }
+
 int main()
 {
     int a[6] = {4,5,6,7,8,9};

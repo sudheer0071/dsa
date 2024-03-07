a9122fb2f641af91e20e5460fb950e9b41c60c39
sudheer0071 <sudheer1614@gmail.com>
2024-03-07 22:53:10 +0530

    final commit of day

--- Char/validPalindrome.cpp
+++ Char/validPalindrome.cpp
@@ -2,45 +2,50 @@
 #include <vector>
 using namespace std;
 
-bool valid(char ch){
-   if ((ch >= 'a'&& ch <='z')||(ch >= 'A'&& ch <='Z')||(ch >= '0'&& ch <='9'))
-   {
-      return 1;
-   }
-   return 0;
-   
-} 
- 
- char toLowerCase(char ch){
-  if (( ch>='a'&&ch<='z')||( ch>='0'&&ch<='9'))
+bool valid(char ch)
+{
+  if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9'))
   {
-    return ch;
-  }else{
-   char temp = ch - 'A' + 'a';
-   return temp;
+    return 1;
   }
-  
+  return 0;
 }
 
-bool checkPalindrone(string a, int n ){
-   int s=0;
-   int e=n-1;
-   while (s<=e)
-   {
+char toLowerCase(char ch)
+{
+  if ((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9'))
+  {
+    return ch;
+  }
+  else
+  {
+    char temp = ch - 'A' + 'a';
+    return temp;
+  }
+}
+
+bool checkPalindrone(string a, int n)
+{
+  int s = 0;
+  int e = n - 1;
+  while (s <= e)
+  {
     if (a[s] != a[e])
     {
       return 0;
     }
-    else{
-      s++;e--;
+    else
+    {
+      s++;
+      e--;
     }
-   }
-    return 1;    
-   
+  }
+  return 1;
 }
 
-bool isPalindrone(string s){
-  int i=0;
+bool isPalindrone(string s)
+{
+  int i = 0;
   string temp = "";
   for (int j = 0; j < s.length(); j++)
   {
@@ -48,20 +53,18 @@ bool isPalindrone(string s){
     {
       temp.push_back(s[j]);
     }
-    
   }
 
-  for (int  j = 0; j < temp.length(); j++)
+  for (int j = 0; j < temp.length(); j++)
   {
     temp[j] = toLowerCase(temp[j]);
   }
   return checkPalindrone(temp, temp.length());
-  
 }
 
 int main()
 {
   string str = "race a car";
-  cout<<isPalindrone(str);
+  cout << isPalindrone(str);
   return 0;
 }

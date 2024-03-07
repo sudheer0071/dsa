b4174db978cbc9f76a1c452ec1eeeef8ce86dd06
sudheer0071 <sudheer1614@gmail.com>
2024-03-07 22:52:40 +0530

    .

--- Char/reverse_string.cpp
+++ Char/reverse_string.cpp
@@ -1,35 +1,35 @@
 #include <iostream>
 #include <vector>
 using namespace std;
- 
 
- 
-void reverse(char name[], int n){
-  int s=0;
-  int e = n-1;
-  while (s<=e)
+void reverse(char name[], int n)
+{
+  int s = 0;
+  int e = n - 1;
+  while (s <= e)
   {
-    swap(name[s++],name[e--]);
+    swap(name[s++], name[e--]);
   }
 }
-int getLen(char name[]){
+int getLen(char name[])
+{
   int count = 0;
-  for (int i = 0; name[i]!='\0'; i++)
+  for (int i = 0; name[i] != '\0'; i++)
   {
     count++;
   }
-   return count;
- }
+  return count;
+}
 
 int main()
 {
   char name[20];
-  cout<<"Enter your name: ";
-  cin>>name; 
- cout<<"Your name is: "<< name<< endl; 
- int len = getLen(name);
- cout<<len<<endl;
- reverse(name, len);
- cout<<name;
+  cout << "Enter your name: ";
+  cin >> name;
+  cout << "Your name is: " << name << endl;
+  int len = getLen(name);
+  cout << len << endl;
+  reverse(name, len);
+  cout << name;
   return 0;
 }

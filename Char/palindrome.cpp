af01f915c9f553b72c9d229ed2c67010aa640716
sudheer0071 <sudheer1614@gmail.com>
2024-03-07 22:51:45 +0530

    new palindrome

--- Char/palindrome.cpp
+++ Char/palindrome.cpp
@@ -1,40 +1,43 @@
 #include <iostream>
 #include <vector>
 using namespace std;
- 
-bool checkPalindrone(char a[], int n ){
-   int s=0;
-   int e=n-1;
-   while (s<=e)
-   {
+
+bool checkPalindrone(char a[], int n)
+{
+  int s = 0;
+  int e = n - 1;
+  while (s <= e)
+  {
     if (a[s] != a[e])
     {
       return 0;
     }
-    else{
-      s++;e--;
+    else
+    {
+      s++;
+      e--;
     }
-   }
-    return 1;    
-   
+  }
+  return 1;
 }
-int getLen(char name[]){
+int getLen(char name[])
+{
   int count = 0;
-  for (int i = 0; name[i]!='\0'; i++)
+  for (int i = 0; name[i] != '\0'; i++)
   {
     count++;
   }
-   return count;
- }
+  return count;
+}
 
 int main()
 {
   char name[20];
-  cout<<"Enter your name: ";
-  cin>>name; 
- cout<<"Your name is: "<< name<< endl; 
- int len = getLen(name);
- cout<<len<<endl;
-cout<<checkPalindrone(name, len);
+  cout << "Enter your name: ";
+  cin >> name;
+  cout << "Your name is: " << name << endl;
+  int len = getLen(name);
+  cout << len << endl;
+  cout << checkPalindrone(name, len);
   return 0;
 }

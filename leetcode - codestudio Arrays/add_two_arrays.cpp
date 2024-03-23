b6d05623400724655bb25886ecc2091952dabbdc
sudheer0071 <sudheer1614@gmail.com>
2024-03-23 23:21:06 +0530

    daily commit

--- leetcode - codestudio Arrays/add_two_arrays.cpp
+++ leetcode - codestudio Arrays/add_two_arrays.cpp
@@ -1,68 +1,68 @@
 #include <iostream>
 #include <vector>
 using namespace std;
- 
-vector<int> reverse(vector<int> arr){
-  int s=0, e=arr.size()-1;
-  int mid = s+(e-s)/2;
-  for (int i = 0; i <=  mid; i++)
+
+vector<int> reverse(vector<int> arr)
+{
+  int s = 0, e = arr.size() - 1;
+  int mid = s + (e - s) / 2;
+  for (int i = 0; i <= mid; i++)
   {
-     swap(arr[s],arr[e]);
-     s++;
-     e--;
+    swap(arr[s], arr[e]);
+    s++;
+    e--;
   }
   return arr;
 }
 
-vector<int> sumArray(vector<int>&a, int n, vector<int>&b, int m ){
-  int i = n-1;
-  int j = m-1;
+vector<int> sumArray(vector<int> &a, int n, vector<int> &b, int m)
+{
+  int i = n - 1;
+  int j = m - 1;
   int carry = 0;
   vector<int> ans;
-  while (i>=0&&j>=0)
+  while (i >= 0 && j >= 0)
   {
-     int val1 = a[i];
-     int val2 = a[j];
+    int val1 = a[i];
+    int val2 = a[j];
 
-     int sum = val1+val2 + carry;
-     carry = sum/10;
-     sum = sum%10;
-     ans.push_back(sum);
-     i--;j--;
+    int sum = val1 + val2 + carry;
+    carry = sum / 10;
+    sum = sum % 10;
+    ans.push_back(sum);
+    i--;
+    j--;
   }
-
 
   // first case
-  while (i>=0)
+  while (i >= 0)
   {
     int sum = carry + a[i];
-      carry = sum/10;
-     sum = sum%10;
-     ans.push_back(sum);
-     i--;
-  } 
+    carry = sum / 10;
+    sum = sum % 10;
+    ans.push_back(sum);
+    i--;
+  }
   // second case
-  while (j>=0)
+  while (j >= 0)
   {
-      int sum = carry + a[j];
-      carry = sum/10;
-     sum = sum%10;
-     ans.push_back(sum);
-     j--;
+    int sum = carry + a[j];
+    carry = sum / 10;
+    sum = sum % 10;
+    ans.push_back(sum);
+    j--;
   }
 
-// third case  
-while (carry!=0) 
-{
-  int sum = carry;
-   carry = sum/10;
-     sum = sum%10;
-     ans.push_back(sum);
+  // third case
+  while (carry != 0)
+  {
+    int sum = carry;
+    carry = sum / 10;
+    sum = sum % 10;
+    ans.push_back(sum);
+  }
+  return reverse(ans);
 }
-return reverse(ans);
- }
-
-
 
 int main()
 {

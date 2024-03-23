300153eb1a03fb2683edac64fd1633855fbae6a8
sudheer0071 <sudheer1614@gmail.com>
2024-03-23 23:20:38 +0530

    daily commit

--- leetcode - codestudio Arrays/merge_2_sorted_arr.cpp
+++ leetcode - codestudio Arrays/merge_2_sorted_arr.cpp
@@ -1,56 +1,57 @@
 #include <iostream>
 using namespace std;
- 
-void merge(int arr1[], int n, int arr2[], int m, int arr3[]){
-  int i=0, j=0;
+
+void merge(int arr1[], int n, int arr2[], int m, int arr3[])
+{
+  int i = 0, j = 0;
   int k = 0;
-  while (i<n && j<m)
+  while (i < n && j < m)
   {
-    if (arr1[i]<arr2[j])
+    if (arr1[i] < arr2[j])
     {
       arr3[k] = arr1[i];
       k++;
       i++;
-    }else{
+    }
+    else
+    {
       arr3[k] = arr2[j];
       k++;
       j++;
     }
-    
   }
-//  copy elements of k in in remaining array of n size
-  while (i<n)
+  //  copy elements of k in in remaining array of n size
+  while (i < n)
   {
     arr3[k] = arr1[i];
     k++;
     i++;
-  }  
-//  copy elements of k in in remaining array of m size
-  while (j<m)
+  }
+  //  copy elements of k in in remaining array of m size
+  while (j < m)
   {
     arr3[k] = arr2[j];
     k++;
     j++;
-  }  
-
+  }
 }
 
-void print(int ans[], int n){
+void print(int ans[], int n)
+{
   for (int i = 0; i < n; i++)
   {
-    cout<<ans[i]<<" ";
-  }cout<<endl;
-  
+    cout << ans[i] << " ";
+  }
+  cout << endl;
 }
 
 int main()
 {
 
-    int a[5] = {1,3,5,7,9};
-    int b[7] = {2,4,6,8,10,11,23};
-    int c[12]={0};
-  merge(a,5, b,7, c);
-  print(c,12);
+  int a[5] = {1, 3, 5, 7, 9};
+  int b[7] = {2, 4, 6, 8, 10, 11, 23};
+  int c[12] = {0};
+  merge(a, 5, b, 7, c);
+  print(c, 12);
   return 0;
 }
- 

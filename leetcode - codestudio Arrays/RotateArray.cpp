932a4224d4932a7d1620b8c43a8186ac13115b62
sudheer0071 <sudheer1614@gmail.com>
2024-03-23 23:21:20 +0530

    daily commit

--- leetcode - codestudio Arrays/RotateArray.cpp
+++ leetcode - codestudio Arrays/RotateArray.cpp
@@ -2,36 +2,36 @@
 #include <vector>
 using namespace std;
 
- 
-void rotate(vector<int>& nums, int k) {
-      
-    
-      int n = nums.size();
-      vector<int> temp(n);
-       
-      for(int i =0;i<n;i++) { 
-        temp[(i+k)%n] = nums[i]; 
-      }
-       
-      for(int i=0;i<nums.size();i++) 
-      {
-        nums[i] = temp[i];
-      } 
-    }
- 
- void printarray(vector<int>arr){
-   for (int i = 0; i < arr.size(); i++)
-   {
-    cout<<arr[i] << " ";
-   }
- }
+void rotate(vector<int> &nums, int k)
+{
+
+  int n = nums.size();
+  vector<int> temp(n);
+
+  for (int i = 0; i < n; i++)
+  {
+    temp[(i + k) % n] = nums[i];
+  }
+
+  for (int i = 0; i < nums.size(); i++)
+  {
+    nums[i] = temp[i];
+  }
+}
+
+void printarray(vector<int> arr)
+{
+  for (int i = 0; i < arr.size(); i++)
+  {
+    cout << arr[i] << " ";
+  }
+}
 
 int main()
 {
-    vector<int>a = {4,5,6,7,8,9};
-  rotate(a,2);
+  vector<int> a = {4, 5, 6, 7, 8, 9};
+  rotate(a, 2);
   printarray(a);
   // cout<<10%6;
   return 0;
-  
 }

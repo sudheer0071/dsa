54275c0a7f721adb5bd40c46a8a83ee217b5a13b
sudheer0071 <sudheer1614@gmail.com>
2024-04-23 21:23:43 +0530

    b

--- 2D_Array/sprint_print.cpp
+++ 2D_Array/sprint_print.cpp
@@ -41,54 +41,54 @@ using namespace std;
 // }
 
 
-  //  vector<int> spiralOrder(vector<vector<int> >& matrix) { 
+   vector<int> spiralOrder(vector<vector<int> >& matrix) { 
         
-  //       vector<int> ans;
-  //       int row = matrix.size();
-  //       int col = matrix[0].size();
+        vector<int> ans;
+        int row = matrix.size();
+        int col = matrix[0].size();
         
-  //       int count = 0;
-  //       int total = row*col;
+        int count = 0;
+        int total = row*col;
         
-  //       //index initialisation
-  //       int startingRow = 0;
-  //       int startingCol = 0;
-  //       int endingRow = row-1;
-  //       int endingCol = col-1;
+        //index initialisation
+        int startingRow = 0;
+        int startingCol = 0;
+        int endingRow = row-1;
+        int endingCol = col-1;
         
         
-  //       while(count < total) {
+        while(count < total) {
             
-  //           //print starting row
-  //           for(int index = startingCol; count < total && index<=endingCol; index++) {
-  //               ans.push_back(matrix[startingRow][index]);
-  //               count++;
-  //           }
-  //           startingRow++;
+            //print starting row
+            for(int index = startingCol; count < total && index<=endingCol; index++) {
+                ans.push_back(matrix[startingRow][index]);
+                count++;
+            }
+            startingRow++;
             
-  //           //print ending column
-  //           for(int index = startingRow; count < total && index<=endingRow; index++) {
-  //               ans.push_back(matrix[index][endingCol]);
-  //               count++;
-  //           }
-  //           endingCol--;
+            //print ending column
+            for(int index = startingRow; count < total && index<=endingRow; index++) {
+                ans.push_back(matrix[index][endingCol]);
+                count++;
+            }
+            endingCol--;
             
-  //           //print ending row
-  //           for(int index = endingCol; count < total && index>=startingCol; index--) {
-  //               ans.push_back(matrix[endingRow][index]);
-  //               count++;
-  //           }
-  //           endingRow--;
+            //print ending row
+            for(int index = endingCol; count < total && index>=startingCol; index--) {
+                ans.push_back(matrix[endingRow][index]);
+                count++;
+            }
+            endingRow--;
             
-  //           //print starting column
-  //           for(int index = endingRow; count < total && index>=startingRow; index--) {
-  //               ans.push_back(matrix[index][startingCol]);
-  //               count++;
-  //           }
-  //           startingCol++;
-  //       } 
-  //       return ans;
-  //   }
+            //print starting column
+            for(int index = endingRow; count < total && index>=startingRow; index--) {
+                ans.push_back(matrix[index][startingCol]);
+                count++;
+            }
+            startingCol++;
+        } 
+        return ans;
+    }
 
 void printarr(int arr[][3], int a, int b){ 
     for (int i = 0; i < a; i++)

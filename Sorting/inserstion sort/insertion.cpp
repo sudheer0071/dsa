103d7d214c27cb38487e3424b272bda43960a7fc
sudheer0071 <sudheer1614@gmail.com>
2024-06-22 09:00:14 +0530

    insertion sort recursive approach

--- Sorting/inserstion sort/insertion.cpp
+++ Sorting/inserstion sort/insertion.cpp
@@ -2,26 +2,39 @@
 #include <vector>
 using namespace std;
  
-void insertion_sort(int arr[], int n){
-  for (int i = 0; i < n; i++)
-  {
-    int j = i;
-    while (j>0 && arr[j-1]>arr[j])
-    {
-    swap(arr[j-1],arr[j]);
-    j--;
-    cout<<"runs";
-    }
+// void insertion_sort(int arr[], int n){
+//   for (int i = 0; i < n; i++)
+//   {
+//     int j = i;
+//     while (j>0 && arr[j-1]>arr[j])
+//     {
+//     swap(arr[j-1],arr[j]);
+//     j--;
+//     cout<<"runs";
+//     }
     
-    // cout<<arr[i]<<' ';
+//     // cout<<arr[i]<<' ';
+//   }
+// }
+ 
+
+//  recursive approach 
+void insertion_sort(int i,int arr[], int n){ 
+  if(i==n) return;
+  int j=i;
+  while (j>0 && arr[j]<arr[j-1])
+  { 
+    swap(arr[j],arr[j-1]); 
+    j--;  
   }
+  insertion_sort(i+1,arr,n);
 }
 
 int main()
 { 
-  // int arr[6] =  {13,46,24,52,20,9};
-  int arr[6] =  {1,2,3,4,5,6};
-  insertion_sort(arr,6);
+  int arr[6] =  {13,46,24,52,20,9};
+  // int arr[6] =  {1,2,3,4,5,6};
+  insertion_sort(0,arr,6);
   for (int i = 0; i < 6; i++)
   {
     cout<<arr[i]<<" ";

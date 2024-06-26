485deb08bd82a5b98453e49fc5509e62258a6735
sudheer0071 <sudheer1614@gmail.com>
2024-06-26 15:52:19 +0530

    done with roated arrays questions

--- Problems on arrays/Easy/rotate_by_k.cpp
+++ Problems on arrays/Easy/rotate_by_k.cpp
@@ -1,35 +1,70 @@
 #include <iostream>
-#include <vector>
+#include <vector> 
 using namespace std;
  
- void left_rotate(int arr[], int n,int k){
-  int temp[n];
-  if (n%2!=0)
+// //  brute force approach
+//  void left_rotate(int arr[], int n,int k){ 
+//   k+k%n;
+//   int temp[n];
+// //   // store in temporary
+// //   for (int i = 0; i <k; i++)
+// //   {
+// //     temp[i] = arr[i];
+// //   }
+
+// // //  arr[] from i = k to last index (for first elements till k)
+// //   for (int  i = k; i < n; i++)
+// //   {
+// //     arr[i-k] = arr[i];
+// //   }
+  
+// //   // adding first first elements of arr[] form temp [] (from i=k to last index)
+// //   for (int i = n-k; i < n; i++)
+// //   {
+// //     arr[i] = temp[i-(n-k)];
+// //   }
+   
+
+// // more optimal brute force
+
+// for (int i = 0; i < n; i++)
+// {
+//   temp[(i+k)%n] = arr[i];
+// }
+// for (int i = 0; i < n; i++)
+// {
+//   arr[i] = temp[i];
+// }
+
+
+// } 
+
+// reversal approach
+
+void reverse(int arr[], int s , int e){
+  while (s<=e)
   {
-    k=k+1;
+    swap(arr[s], arr[e]);
+    s++;
+    e--;
   }
-  // cout<<"k = "<<k;
-  for (int i = 0; i <n/2; i++)
-  {   
-    temp[k+i-1] = arr[i];
-    // cout<<"temp"<<"["<<k+i<<"]: "<<temp[k+i]<<" ";
-    temp[i] = arr[k+i]; 
-  }
-arr=temp;
-  for (int i = 0; i < n; i++)
-{
-  cout<<arr[i]<<" ";
+  
 }
- }
+
+void left_rotate(int arr[], int n, int k){
+  reverse(arr, 0, k);
+  reverse(arr, k+1, n-1);
+  reverse(arr, 0, n-1);
+}
 
 int main()
 {
   int arr[7] = {1,2,3,4,5,6,7};
 left_rotate(arr,7,3);
-// for (int i = 0; i < 6; i++)
-// {
-//   cout<<arr[i]<<" ";
-// }
-
+// cout<<3%4;
+for(int i = 0; i < 7; i++)
+{
+  cout<<arr[i]<<" ";
+} 
   return 0;
 }

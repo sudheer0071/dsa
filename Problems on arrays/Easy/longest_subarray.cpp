f0826e78abb214a4ab240dd19bf4d39451ce122b
sudheer0071 <sudheer1614@gmail.com>
2024-07-03 22:38:31 +0530

    tried this queston all day i fucked up

--- Problems on arrays/Easy/longest_subarray.cpp
+++ Problems on arrays/Easy/longest_subarray.cpp
@@ -3,24 +3,45 @@
 using namespace std;
  
 int longest_subarray(int arr[],int n, int k){
-  int idx, count=0;
+  int idx=0, count=0;
   int sum = 0;
   bool flag=false;
+  int temp=0;
   for (int i = 0; i < n; i++)
   {
-    if (sum>=k||arr[i]==0)
-    {
-      flag = true;
-      idx = count;
+    if (sum==k||arr[i]==0)
+    { 
+      flag = true; 
+
+      cout<<"idx = "<<idx<<endl;
+      if (count>=idx)
+      {
+      idx = count;   
+      }
+      temp = idx;
       count=0;
       sum=0;
     }
-    sum+=arr[i];
+    if (sum>k)
+    { 
+      cout<<"insidee"<<endl;
+       if (arr[i]>0)
+       {
+         flag = true;
+      idx = 0;
+      count=0;
+      sum=0;
+       } 
+    } 
+    
+    sum+=arr[i]; 
+    count++; 
     cout<<"sum = "<<sum<<endl;
-    count++;
+    cout<<"count = "<<count<<endl;
   }
   idx = max(idx, count);   
-  return flag?idx:0;
+  cout<<"flag = "<<flag<<endl; 
+  return flag&&temp?idx:0;
 }
 
 int main()
@@ -30,8 +51,11 @@ int main()
 //  int arr[10] =  {-7,-11,-3,-7,4,15,-13,18,-10,-10};
 //  int arr[17] =  {-13,0,6,15,16,2,15,-12,17,-16,0,-3,19,-3,2,-9,-6};
 //  int arr[17] =  {-1,2,3}; 
- int arr[8] =  {8,-9,10,-2,-10,6,18,17}; 
+//  int arr[8] =  {8,-9,10,-2,-10,6,18,17}; 
+//  int arr[13] =  {0,-16,-19,-12,6,12,-11,-15,-3,-9,6,-19,12}; 
+ int arr[8] =  {1,2,1,2,1}; 
 
-  cout<< longest_subarray(arr,8,17);
+
+  cout<< longest_subarray(arr,6,4);
   return 0;
 }

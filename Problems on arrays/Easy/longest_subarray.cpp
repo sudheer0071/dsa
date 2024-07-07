d91739a4131043a1c318c386f8605de6f0dbb42b
sudheer0071 <sudheer1614@gmail.com>
2024-07-07 16:41:26 +0530

    ./

--- Problems on arrays/Easy/longest_subarray.cpp
+++ Problems on arrays/Easy/longest_subarray.cpp
@@ -22,77 +22,81 @@ using namespace std;
 // }
  
 // My approach 
-// int longest_subarray(int arr[],int n, int k){
-//   int idx=0, count=0;
-//   int sum = 0;
-//   bool flag=false;
-//   int temp=0;
-//   for (int i = 0; i < n; i++)
-//   {
-//     sum+=arr[i]; 
-//     count++; 
-//     cout<<"sum = "<<sum<<endl;
-//     cout<<"count = "<<count<<endl; 
-//     if (sum==k||arr[i]==0)
-//     { 
-//     if (arr[i+1]==0)
-//     {
-//       count++;
-//     }
-//    else{
-//       flag = true;  
-//       cout<<"idx = "<<idx<<endl;
-//       if (count>=idx)
-//       {
-//       idx = count;
-//       }
-//       // i=i-1;   
-//       temp = idx;
-//       count=0;
-//       sum=0;
-//    }
-//     }
-//     if (sum>k)
-//     {  
-//       cout<<"insidee"<<endl;
-//        if (arr[i]>0)
-//        {
-//          flag = true; 
-//       idx = 0;
-//       count=0;
-//       sum=0;
-//        } 
-//        idx=temp;
-//     }  
-//   }
-//   idx = max(idx, count);   
-//   cout<<"temp = "<<temp<<endl;  
-//   return flag?idx:0;
-// }
- 
-// Optimal Approach
-int longest_subarray(int arr[], int n, int k){
-  int l=0,r=0;
-  int maxlen=0;
-  int sum = arr[0];
-  while (r<n)
+int longest_subarray(int arr[],int n, int k){
+  int idx=0, count=0;
+  int sum = 0;
+  bool flag=false;
+  int temp=0;
+  for (int i = 0; i < n; i++)
   {
-    while (sum>k&&l<=r)
+    sum+=arr[i]; 
+    count++; 
+    cout<<"sum = "<<sum<<endl;
+    cout<<"count = "<<count<<endl; 
+    if (sum==k||arr[i]==0)
+    { 
+    if (arr[i+1]==0)
     {
-      sum-=arr[l];
-      l++;
+      count++;
     }
-    if (sum==k)
-    {
-      maxlen = max(maxlen, r-l+1);
+   else{
+      flag = true;  
+      cout<<"idx = "<<idx<<endl;
+      if (count>=idx)
+      {
+      idx = count;
+      }
+      // i=i-1;   
+      temp = idx;
+      count=0;
+      sum=0;
+   }
     }
-    r++;
-    if (r<n){
-      sum+=arr[r];
+    if (sum>k)
+    {  
+      cout<<"insidee"<<endl;
+       if (arr[i]>0)
+       {
+         flag = true; 
+      idx = 0;
+      count=0;
+      sum=0;
+       } 
+       idx=temp;
     }  
   }
-  return maxlen;
+  idx = max(idx, count);   
+  return flag?idx:0;
 }
+ 
+// Optimal Approach 1
+// int longest_subarray(int arr[], int n, int k){
+//   int l=0,r=0;
+//   int maxlen=0;
+//   int sum = arr[0];
+//   while (r<n)
+//   {
+//     while (sum>k&&l<=r)
+//     {
+//       sum-=arr[l];
+//       l++;
+//     }
+//     if (sum==k)
+//     {
+//       maxlen = max(maxlen, r-l+1);
+//     }
+//     r++;
+//     if (r<n){
+//       sum+=arr[r];
+//     }  
+//   }
+//   return maxlen;
+// }
+
+// Optimal Approach 2
+// int longest_subarray(int arr[], int n, int k){
+  
+// }
 
 int main()
 {
@@ -104,10 +108,12 @@ int main()
 //  int arr[8] =  {8,-9,10,-2,-10,6,18,17}; 
 //  int arr[13] =  {0,-16,-19,-12,6,12,-11,-15,-3,-9,6,-19,12}; 
 //  int arr[8] =  {1,2,1,2,1}; 
- int arr[10] =  {1,2,3,1,1,1,1,4,2,3}; 
+//  int arr[10] =  {1,2,3,1,1,1,1,4,2,3}; 
+int arr[3] = {-1,1,1};
 // int arr[4] = {2,0,0,3};
 
 
-  cout<< longest_subarray(arr,10,7);
+  cout << longest_subarray(arr,3,1);
   return 0;
 }
+ 

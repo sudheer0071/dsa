c9facf5b9a86ae05f965d974c16e8dd5ed088c49
sudheer0071 <sudheer1614@gmail.com>
2024-07-11 18:21:23 +0530

    not solving

--- Problems on arrays/medium/sort_0_1_2.cpp
+++ Problems on arrays/medium/sort_0_1_2.cpp
@@ -10,24 +10,26 @@ void sort_0_1_2(int arr[], int n){
     if (arr[i]==0)
     {
       swap(arr[i],arr[l]);
-      l++;
+      l++; 
+      i++;
     }
     else if (arr[i] == 2)
     {
       swap(arr[i],arr[r]);
-      r--;
-    }
-    i++;
-    
-  }
-  
+      r--; 
+      i++;
+    }  else{
+    i++;  
+    } 
+  } 
 }
  
 int main()
 {
-  int arr[6] = {2,0,2,1,1,0};
+  // int arr[6] = {2,0,2,1,1,0};
+  int arr[6] = {2,0,2,1,2,0};
  sort_0_1_2(arr,6);
-  for (int i = 0; i < 6; i++)
+  for (int i = 0; i <6; i++)
   {
     cout<<arr[i]<<" ";
   }

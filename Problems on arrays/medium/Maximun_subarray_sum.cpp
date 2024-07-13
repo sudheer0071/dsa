a0c7719d96718a3cdbb5ce4fc9429c4b4df10f0d
sudheer0071 <sudheer1614@gmail.com>
2024-07-13 18:05:01 +0530

    Kadane's Algorithm, maximum subarray sum O(n) TC 67 ms runtime constant SC

---
+++ Problems on arrays/medium/Maximun_subarray_sum.cpp
@@ -0,0 +1,64 @@
+ 
+#include <iostream>
+#include <vector>
+#include <map>  
+// #include <bits/stdc++.h>
+using namespace std;
+
+// Brute force Approach 
+// int maximum_subarray_sum(int arr[], int n){
+//   int maxi = INT32_MIN;
+//   for (int i = 0; i < n; i++)
+//   {
+//     for (int j = i; j < n; j++)
+//     {
+//   int sum = 0;
+//       for (int k = i; k <=j; k++)
+//       {
+//         sum+=arr[k];
+//       }
+//         maxi = max(sum, maxi); 
+//     } 
+//   }
+//   return maxi;
+// }
+ 
+// Better Approach - Reducing one loop in Brute force approach
+// int maximum_subarray_sum(int arr[], int n){
+//   int maxi = INT32_MIN;
+//   for (int i = 0; i < n; i++)
+//   {
+//   int sum = 0;
+//     for (int j = i; j < n; j++)
+//     {
+//      sum+=arr[j];
+//   maxi = max(sum, maxi);
+//   }
+// }
+//   return maxi;
+// }
+
+// Optimal Approach - Using Kadane's Algorightm
+int maximum_subarray_sum(int arr[], int n){
+  int maxi = INT32_MIN;
+  int sum = 0; 
+  for (int i = 0; i < n; i++)
+  {
+    sum+=arr[i];
+    maxi = max(sum, maxi);
+    if(sum<0) sum=0;
+}
+  return maxi;
+}
+
+
+int main()
+{
+//  int arr[7] = {2,2,1,1,1,2,2};
+//  int arr[3] = {3,2,3};
+//  int arr[10] = {-2,1,-3,4,-1,2,1,-5,4};
+ int arr[10] = {-2,-3,4,-1,-2,1,5,-3};
+ cout<<maximum_subarray_sum(arr,10); 
+  return 0;
+}
+ 

edbf7f4a2eb59c95efb61cd14d219fcf415f30ef
sudheer0071 <sudheer1614@gmail.com>
2024-07-24 18:35:07 +0530

    set zeros by optimal approach

--- Problems on arrays/medium/set_zero_matrix.cpp
+++ Problems on arrays/medium/set_zero_matrix.cpp
@@ -45,48 +45,100 @@ void markCol(vector<vector<int>> &matrix, int n, int m, int j) {
 //     } 
 //     return matrix;
 // }
-vector<vector<int>> zeroMatrix(vector<vector<int>> &arr, int n, int m) {
- 
- int row[n]={0}, col[m]={0};
-    // Set -1 for rows and cols
-    // that contains 0. Don't mark any 0 as -1:
 
+// Better Approach
+// vector<vector<int>> zeroMatrix(vector<vector<int>> &arr, int n, int m) { 
+//  int row[n]={0}, col[m]={0}; 
+//     for (int i = 0; i < n; i++) {
+//         for (int j = 0; j < m; j++) {
+//             if (arr[i][j] == 0) {
+//                  row[i]=1;
+//                  col[j]=1;
+//             } 
+//         }
+//     } 
+//     // Finally, mark all -1 as 0:
+//     for (int i = 0; i < n; i++) {
+//         for (int j = 0; j < m; j++) {
+//             if (arr[i][j]!= 0) {
+//                 if (row[i] || col[j])
+//                 {
+//                   arr[i][j]=0;
+//                 } 
+//             }
+//         }
+//     } 
+//     return arr;
+// }
+
+
+// Optimal Approach
+ vector<vector<int>> zeroMatrix(vector<vector<int>> &arr, int n, int m) {
+    int row[n]={0}, col[m]={0}; 
+     // int row[n] = {0}; --> matrix[..][0]
+    // int col[m] = {0}; --> matrix[0][..]
+
+   int col0=1;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             if (arr[i][j] == 0) {
-                 row[i]=1;
-                 col[j]=1;
+                 arr[i][0]=0;
+                 if(j!=0)
+                 arr[0][j]=0;
+                 else
+                  col0 = 0;
             } 
         }
-    }
+    }     
 
-    // Finally, mark all -1 as 0:
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (arr[i][j]!= 0) {
-                if (row[i] || col[j])
+     for (auto it : arr) {
+        for (auto ele : it) {
+            cout << ele << " ";
+        }
+        cout <<endl;
+    }
+        cout <<endl;
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j < m; j++)
+        {
+            if (arr[i][j]!=0)
+            {
+                if (arr[i][0]==0|| arr[0][j]==0)
                 {
-                  arr[i][j]=0;
+                    arr[i][j]=0;
                 }
-                
             }
         }
-    }
-
+    } 
+    if (arr[0][0]==0)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            arr[0][j]=0;
+        } 
+    } 
+    if (col0==0)
+    {
+      for (int i = 0; i < n; i++)
+      {
+        arr[i][0]=0;
+      } 
+    } 
     return arr;
-}
+ }
+
 
 int main()
 {
-    vector<vector<int>> matrix = {{1, 1, 1,1}, {1, 0, 0,1},{1,1,0,1}, {1, 1, 1,1}};
+    vector<vector<int>> matrix = {{0, 1, 1,1}, {1, 0, 0,1},{1,1,0,1}, {1, 1, 1,1}};
     int n = matrix.size();
     int m = matrix[0].size();
     vector<vector<int>> ans = zeroMatrix(matrix, n, m);
      for (auto it : ans) {
         for (auto ele : it) {
             cout << ele << " ";
-        }
-        cout <<endl;
+        } 
     }
     return 0;
 }

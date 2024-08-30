e878c783f038e12832ba4f38831b21b0866160c8
sudheer0071 <sudheer1614@gmail.com>
2024-08-30 13:49:11 +0530

    aggressive cow Optimal solution

--- Binary Search - striver/aggressive_cows.cpp
+++ Binary Search - striver/aggressive_cows.cpp
@@ -3,30 +3,50 @@
 #include <algorithm>
 using namespace std;
 
-bool canWePlace(int arr[], int d, int k){
-
+bool canWePlace(int arr[], int n, int dist, int k){
+   int count =1; int last = arr[0];
+    for (int j = 1; j < n; j++)
+    {
+      if (arr[j]-last>=dist)
+      {
+        count++;
+        last = arr[j];
+      }
+      if(count>=k) return true; 
+    }
+    return  false;
 }
 
-// Brute Force Approach - Linear Search
+// Brute Force Approach - linear search
+// int min_max_dist(int arr[], int n, int k){
+//   sort(arr, arr+n);
+//   int max_d = arr[n-1]- arr[0];
+//   for (int i = 1; i < max_d; i++)
+//   {
+//    if(canWePlace(arr, n, i, k)==false) return i-1;
+//   }
+//   return max_d;
+// }
+
+// Optimal Approach - Binear Search
 int min_max_dist(int arr[], int n, int k){
   sort(arr, arr+n);
-  int limit = arr[n-1] - arr[0];
-  cout<<limit<<endl;
-  for (int i = 1; i < limit; i++)
+  int max_d = arr[n-1]- arr[0];
+  int s = 1; int e = max_d;
+  int ans  = max_d;
+  while(s<=e)
   {
-    int count = 1; int last = arr[0];
-      for (int j = 1; j < n; j++)
-      {
-        if (arr[j]-last>=i)
-        {
-          count++;
-          last = arr[j];
-        }
-        if(count>=k) break;
-      }
-      if(count<k) return i-1;
+    int mid = s + (e-s)/2;
+   if(canWePlace(arr, n, mid, k)==false) {
+
+   e = mid -1;
+    ans = mid -1;
+    }
+   else {
+    s = mid+1;
+   }
   }
-  return limit;
+  return ans;
 }
 
 int main()
